76993a895988a554fad4cef392478bb1e04e9c17
Harsh Solanki <hsolanki1884@gmail.com>
2021-05-03 15:38:06 +0530

    no progress today

---
+++ 450_questions/175leftv.cpp
@@ -0,0 +1,29 @@
+//Function to return a list containing elements of left view of the binary tree.
+vector<int> leftView(Node *root)
+{
+   // Your code here
+   vector<int> v;
+   if(root==NULL){
+       return v;
+   }
+   queue<Node *> q;
+   q.push(root);
+   while(!q.empty()){
+       int n=q.size();
+       for(int i=1;i<=n;i++){
+           Node *temp=q.front();
+           q.pop();
+           if(i==1){
+               v.push_back(temp->data);
+           }
+           if(temp->left){
+                q.push(temp->left);
+            }
+            if(temp->right){
+                q.push(temp->right);
+            }
+       }
+   }
+   return v;
+   
+}
---
+++ 450_questions/52reverseString.cpp
@@ -0,0 +1,11 @@
+ void reverseString(vector<char>& s) {
+      int i=0,j=s.size()-1;
+        while(i<j){
+            char temp=s[i];
+            s[i]=s[j];
+            s[j]=temp;
+            i++;
+            j--;
+        }
+        
+    }
